027c2332476296f1934913ac6d137419bc7821d9
Bruce Lam <xiaohuilam@users.noreply.github.com>
2024-06-03 09:31:41 +0800

    Update CertificateUpdateDcvRequest.php

--- src/Requests/CertificateUpdateDcvRequest.php
+++ src/Requests/CertificateUpdateDcvRequest.php
@@ -6,9 +6,10 @@ namespace QuantumCA\Sdk\Requests;
  * 更改DCV接口请求
  *
  * @property string $service_id 必传,下单时返回的id
- * @property string $domain 必传,域名
- * @property string $type 必传,验证类型:dns/http/https/email
- * @property string $value 当 type=email 时候必传,邮箱地址
+ * @property string $domain_dcv 
+ * @property string $domain [不推荐]必传,域名 
+ * @property string $type [不推荐]必传,验证类型:dns/http/https/email
+ * @property string $value [不推荐]当 type=email 时候必传,邮箱地址
  */
 class CertificateUpdateDcvRequest extends AbstractRequest
 {
