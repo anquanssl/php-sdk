c43cd6386e187328bdf88c5ef401581d6106b2d6
Bruce Lam <xiaohuilam@users.noreply.github.com>
2025-02-19 01:17:05 +0800

    Update CertificateRefundScheme.php

--- src/Scheme/CertificateRefundScheme.php
+++ src/Scheme/CertificateRefundScheme.php
@@ -5,7 +5,7 @@ namespace QuantumCA\Sdk\Scheme;
 /**
  * 订单退款格式
  *
- * @property integer $service_id 帝玺的订单号
+ * @property integer $service_id 我司证书编号
  * @property string $status refunded
  */
 class CertificateRefundScheme
