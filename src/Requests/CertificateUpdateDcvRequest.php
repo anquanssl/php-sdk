191315299795bb6ebd0e4e54796ef32a672f75c6
Bruce Lam <xiaohuilam@users.noreply.github.com>
2025-02-19 01:23:58 +0800

    Update CertificateUpdateDcvRequest.php

--- src/Requests/CertificateUpdateDcvRequest.php
+++ src/Requests/CertificateUpdateDcvRequest.php
@@ -8,8 +8,7 @@ namespace QuantumCA\Sdk\Requests;
  * @property string $service_id 必传,下单时返回的id
  * @property object|array<string,string>|array<string,"dns"|"http"|"https"> $domain_dcv 
  * @deprecated @property string $domain [不推荐]必传,域名 
- * @deprecated @property string $type [不推荐]必传,验证类型:dns/http/https/email
- * @deprecated @property string $value [不推荐]当 type=email 时候必传,邮箱地址
+ * @deprecated @property string $type [不推荐]必传,验证类型:dns/http/https
  */
 class CertificateUpdateDcvRequest extends AbstractRequest
 {
